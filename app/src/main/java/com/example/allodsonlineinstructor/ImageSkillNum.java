57988d9a3fd51799d981a4c6068c458cd0d469e9
Nick-labs <bakatarega@mail.ru>
2022-04-07 18:12:42 +0300

    isee

--- app/src/main/java/com/example/allodsonlineinstructor/ImageSkillNum.java
+++ app/src/main/java/com/example/allodsonlineinstructor/ImageSkillNum.java
@@ -2,9 +2,11 @@ package com.example.allodsonlineinstructor;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Point;
+import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
@@ -19,11 +21,24 @@ public class ImageSkillNum extends View {
         SkillNum.setText(a);
     }
 
+    public ImageSkillNum(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ImageSkillNum(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public ImageSkillNum(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
     @Override
     protected void onDraw(Canvas canvas) {
         Paint p = new Paint();
-        canvas.drawRect(0,0,50,50,p);
-        p.setColor(INVISIBLE);
+        canvas.drawRect(0,0,100,100,p);
+        p.setColor(Color.WHITE);
+        p.setTextSize(100);
         canvas.drawText(a + "",45,45,p);
         super.onDraw(canvas);
     }
